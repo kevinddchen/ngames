961b252eb5bcbbd7a244f7e556dda2441547b0b1
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2025-02-21 21:58:37 -0800

    Reorganize

--- src/ui.h
+++ src/ui.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <ncurses.h>
-
 
 namespace mines
 {
@@ -21,29 +19,6 @@ constexpr short COLOR_PAIR_UNOPENED = 8;  // same color as "8" cell
 /**
  * Initialize ncurses.
  */
-inline void init_ncurses()
-{
-    initscr();
-    cbreak();
-    noecho();
-
-    start_color();
-
-    // define grey color
-    init_color(COLOR_GREY, 500, 500, 500);
-
-    // color pairs for cell numbers
-    init_pair(1, COLOR_BLUE, COLOR_BLACK);
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);
-    init_pair(3, COLOR_RED, COLOR_BLACK);
-    init_pair(4, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
-    init_pair(6, COLOR_CYAN, COLOR_BLACK);
-    init_pair(7, COLOR_WHITE, COLOR_BLACK);
-    init_pair(8, COLOR_GREY, COLOR_BLACK);
-
-    // color pair for mines
-    init_pair(COLOR_PAIR_MINES, COLOR_WHITE, COLOR_RED);
-}
+void init_ncurses();
 
 }  // namespace mines

3a28a9072b9285bc84bfec09f1c2c82d6caaf5fd
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2025-02-22 00:25:42 -0800

    Allow arrow keys for nagivation, resolves #6

--- src/app.cpp
+++ src/app.cpp
@@ -26,8 +26,7 @@ public:
           text_mine_count(board, MARGIN_TOP, MARGIN_LEFT),        // height is 1
           board(rows, cols, mines, MARGIN_TOP + 1, MARGIN_LEFT),  // height is rows + 2 * BORDER_WIDTH
           text_end_game(board, MARGIN_TOP + 1 + rows + 2 * BORDER_WIDTH, MARGIN_LEFT),  // height is 1
-          // 1 row buffer
-          text_instructions(MARGIN_TOP + 3 + rows + 2 * BORDER_WIDTH, MARGIN_LEFT)
+          text_instructions(MARGIN_TOP + 2 + rows + 2 * BORDER_WIDTH, MARGIN_LEFT)
     {
     }
 
@@ -36,6 +35,9 @@ public:
      */
     void run()
     {
+        // Allow arrow keys
+        keypad(board.window, true);
+
         // Initial print
         text_mine_count.refresh();
         board.refresh();
@@ -44,7 +46,7 @@ public:
 
         while (true) {
             wmove(board.window, cursor_y + BORDER_WIDTH, cursor_x + BORDER_WIDTH);
-            const char key = wgetch(board.window);
+            const int key = wgetch(board.window);
             handle_keystroke(key);
         }
 
@@ -55,25 +57,29 @@ public:
      * Perform action associated with given keystroke.
      * @param key Key pressed.
      */
-    void handle_keystroke(char key)
+    void handle_keystroke(int key)
     {
         switch (key) {
             case 'h':
+            case KEY_LEFT:
                 if (cursor_x > 0) {
                     --cursor_x;
                 }
                 break;
             case 'j':
+            case KEY_DOWN:
                 if (cursor_y < board.rows - 1) {
                     ++cursor_y;
                 }
                 break;
             case 'k':
+            case KEY_UP:
                 if (cursor_y > 0) {
                     --cursor_y;
                 }
                 break;
             case 'l':
+            case KEY_RIGHT:
                 if (cursor_x < board.cols - 1) {
                     ++cursor_x;
                 }
--- src/text_instructions.cpp
+++ src/text_instructions.cpp
@@ -6,18 +6,18 @@
 namespace mines
 {
 
-TextInstructions::TextInstructions(int start_y, int start_x) : Component(newwin(6, 16, start_y, start_x)) {}
+TextInstructions::TextInstructions(int start_y, int start_x) : Component(newwin(6, 24, start_y, start_x)) {}
 
 void TextInstructions::refresh() const
 {
     const auto attr = COLOR_PAIR(COLOR_PAIR_INSTRUCTIONS);
     wattron(window, attr);
-    mvwprintw(window, 0, 0, "  h: move left");
-    mvwprintw(window, 1, 0, "  j: move down");
-    mvwprintw(window, 2, 0, "  k: move up");
-    mvwprintw(window, 3, 0, "  l: move left");
-    mvwprintw(window, 4, 0, "  f: place flag");
-    mvwprintw(window, 5, 0, "' ': open cell");
+    mvwprintw(window, 0, 0, " h/left: move left");
+    mvwprintw(window, 1, 0, " j/down: move down");
+    mvwprintw(window, 2, 0, "   k/up: move up");
+    mvwprintw(window, 3, 0, "l/right: move right");
+    mvwprintw(window, 4, 0, "      f: place flag");
+    mvwprintw(window, 5, 0, "  space: open cell");
     wattroff(window, attr);
     wrefresh(window);
 }
